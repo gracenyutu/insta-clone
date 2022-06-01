bd711058e917ce35b015e9eb00d42b658088b227
Grace Nyutu <graceynyutu@gmail.com>
2022-06-01 13:31:31 +0300

    create image and profile models

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    bio = models.CharField(max_length=100, null=False, blank=False)
+    photo = models.ImageField(null=False, default='default.jpg')
+
+    def __str__(self):
+        return self.bio
+
+class Image(models.Model):
+    image = models.ImageField(null=False, default='default.jpg')
+    name = models.CharField(max_length=100, null=False, blank=False)
+    caption = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    likes = models.CharField(max_length=100, null=False, blank=False)
+    comments = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.caption
