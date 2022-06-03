99d3d6164964deef7f2ebf2220c4855ba97ea460
Grace Nyutu <graceynyutu@gmail.com>
2022-06-03 17:49:32 +0300

    create login,profile and createprofile paths

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-     path('search/', views.search, name='search'),
+    path('search/', views.search, name='search'),
+    path('createprofile/',views.create_profile,name="signup"),
+    path('login/',views.Login,name="Login"),
+    path('profile/',views.profile,name="profile")
 ]
