704c9ab2f406400e8e6be1270e2efd8d1e2e130a
Grace Nyutu <graceynyutu@gmail.com>
2022-06-03 21:40:14 +0300

    create model named Profile

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    bio = models.CharField(max_length=100, null=False, blank=False)
-    photo = models.ImageField(null=False, default='default.jpg')
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    followers = models.ManyToManyField(User,related_name="followers",blank=True)
+    followings = models.ManyToManyField(User,related_name="followings")
+    photo = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.bio
--- instaapp/views.py
+++ instaapp/views.py
@@ -5,18 +5,18 @@ from .models import Profile, Image
 def home(request):
     return render(request, 'instaapp/home.html')
 
-def profile(request,id=None):
-    if not request.user.is_authenticated:
-        return redirect("Login")
-    if id is not None:
-        profile_id = Profile.objects.get(id=id)
-        profile = Profile.objects.get(user=request.user)
-        profileimage = profile.profile_picture.url
-    else:
-        profile_id = Profile.objects.get(user=request.user)
-        profile = Profile.objects.get(user=request.user)
-        profileimage = profile.profile_picture.url
-    return render(request,'profile.html',{'profile':profile_id,'profileimage':profileimage,'profile_of_user':True})
+def profile(request):
+    # if not request.user.is_authenticated:
+    #     return redirect("Login")
+    # if id is not None:
+    #     profile_id = Profile.objects.get(id=id)
+    #     profile = Profile.objects.get(user=request.user)
+    #     profileimage = profile.profile_picture.url
+    # else:
+    #     profile_id = Profile.objects.get(user=request.user)
+    #     profile = Profile.objects.get(user=request.user)
+    #     profileimage = profile.profile_picture.url
+    return render(request,'instaapp/profile.html')
 
 def search(request):
     if not request.user.is_authenticated:
@@ -28,4 +28,10 @@ def search(request):
     search = request.GET['username']
     profiles = Profile.objects.filter(user__username__icontains=search)
     context = {'profiles':profiles,'username':search,"profileimage":profileimage}
-    return render(request,'search.html',context)
+    return render(request,'instaapp/search.html',context)
+
+def Login(request):
+    return render(request, 'instaapp/login.html')
+
+def create_profile(request):
+    return render(request, 'instaapp/signup.html')
